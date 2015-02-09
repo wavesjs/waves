83c2deb39fdaf20547fb37f58efdb53f1956f275
b-ma <benjamin.matuszewski@gmail.com>
2015-02-09 15:19:08 +0100

    reorder namespaces - updates waveform

--- waves.js
+++ waves.js
@@ -10,17 +10,16 @@ waves.ui = {
   segment: require('segment'),
   marker: require('marker'),
   breakpoint: require('breakpoint'),
-  label: require('label')
-};
-
-waves.helpers = {
+  label: require('label'),
   zoomer: require('zoomer'),
-  loaders: require('loaders')
+  utils: require('utils')
 };
 
-waves.utils = require('utils');
+waves.loaders = require('loaders');
 
 waves.audio = {};
+
+waves.lfo = {};
 
 // expose d3;
 waves.d3 = waves.ui.timeline.d3;
